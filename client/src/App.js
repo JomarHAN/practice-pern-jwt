8d10be1e71a757677d6cce21207129cd407be9f9
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-01-31 17:15:09 -0500

    keep continue on

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
+import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Switch>
-          <h1>Hello</h1>
-        </Switch>
-      </div>
-    </Router>
+    <Fragment>
+      <Router>
+        <div className="app">
+          <div className="app__nav">
+            <div className="container app__header">
+              <div className="app__headerLeft">
+                <Link to="/">
+                  <h1>Home</h1>
+                </Link>
+              </div>
+              <div className="app_headerRight">
+                <Link>
+                  <h3>Login</h3>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
