38463417c2c28674753f801df04e6c961e67d77a
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-04 15:06:10 -0500

    ready to upload into cloud

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(cors())
 
 //register and login
