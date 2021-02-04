30c6fae5cd762ec93ce9573b31b7c40605246564
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-03 23:48:46 -0500

    fix getuserName on dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useStateValue } from "../ContextAPI/StateProvider";
 
 function Dashboard() {
   const [{ isAuth }] = useStateValue();
-  return isAuth ? <h1>Dashboard here</h1> : <h1>you need to login</h1>;
+  const [userName, setUserName] = useState("");
+  const getUserName = async () => {
+    const response = await fetch("http://localhost:5000/dashboard", {
+      method: "GET",
+      headers: { token: localStorage.token },
+    });
+    const parseRes = await response.json();
+    setUserName(parseRes.user_name);
+  };
+
+  useEffect(() => {
+    getUserName();
+  }, []);
+  return isAuth ? <h1>Welcome {userName}</h1> : <h1>you need to login</h1>;
 }
 
 export default Dashboard;
