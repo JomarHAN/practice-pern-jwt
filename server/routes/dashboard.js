7cf0666f525bd18b1391b9720a0b499caa0c5a1f
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-04 20:56:07 -0500

    fix login bugs

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,6 +2,7 @@ const authentication = require('../middleware/authentication');
 const router = require('express').Router();
 const pool = require('../pgConfig')
 
+//get user name
 router.get('/', authentication, async (req, res) => {
     try {
         const userName = await pool.query("SELECT user_name FROM usersImage WHERE user_id = $1", [req.user])
@@ -10,6 +11,16 @@ router.get('/', authentication, async (req, res) => {
     } catch (error) {
         console.error(error.message)
         res.status(500).json("Server Error")
+    }
+})
+
+//upload images
+router.post('/upload', async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ msg: "Something went wrong!!!" })
     }
 })
 
